75a8f8a70b2a87f12fa8a2acb36e8ce059aa4256
martinizin <mar45pillacela@gmail.com>
2024-06-12 17:28:20 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Blog',
+  webDir: 'www'
+};
+
+export default config;
