496ffc7412f23040638391d5abca230770bf0cf6
martinizin <mar45pillacela@gmail.com>
2024-06-16 01:47:51 -0500

    Ocultado de credenciales

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,14 +1,5 @@
 export const environment = {
   production:true,
-  firebaseConfig: {
-  apiKey: "AIzaSyDMiACfksFb3PP6C25GrnO47c7PoFGYhzA",
-  authDomain: "examenam1b.firebaseapp.com",
-  projectId: "examenam1b",
-  storageBucket: "examenam1b.appspot.com",
-  messagingSenderId: "996058089766",
-  appId: "1:996058089766:web:1d1c82e2c75a1a4b1ad9e7",
-  measurementId: "G-SK0NPLBJED"
-  }
-  
+   //TODO: COLOCAR AQUÍ TUS CREDENCIALES DE TU PROYECTO FIREBASE
 
   };
